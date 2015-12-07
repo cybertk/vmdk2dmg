326a9d05a7f74e6ab447f5344e7e4ba5e36c01cb
Quanlong <kyan.ql.he@gmail.com>
2015-12-07 11:47:53 +0800

    [homebrew] Add recommended VirtualboxRequirement

--- vmdk2dmg.rb
+++ vmdk2dmg.rb
@@ -9,6 +9,17 @@ class ParagonVMDKMounterRequirement < Requirement
   end
 end
 
+class VirtualboxRequirement < Requirement
+  fatal true
+
+  satisfy { which("VBoxManage") }
+
+  def message; <<-EOS.undent
+    "virtuabox is recommended to install in order to convert from VM name directly. Consider `brew install Caskroom/cask/virtualbox`."
+    EOS
+  end
+end
+
 class Vmdk2dmg < Formula
   desc "Convert vmdk to dmg"
   homepage "https://github.com/cybertk/vmdk2dmg"
@@ -19,6 +30,7 @@ class Vmdk2dmg < Formula
   head "https://github.com/cybertk/vmdk2dmg.git"
 
   depends_on ParagonVMDKMounterRequirement
+  depends_on VirtualboxRequirement => :recommended
 
   def install
     bin.install "vmdk2dmg"
